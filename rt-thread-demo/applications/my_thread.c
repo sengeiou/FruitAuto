0d9608177c55fd307a306c5c260ad1849467750e
KaiJin <ijink@qq.com>
2022-03-29 15:02:25 +0800

    2022/3/29

--- rt-thread-demo/applications/my_thread.c
+++ rt-thread-demo/applications/my_thread.c
@@ -9,7 +9,7 @@
  */
 #include <rtthread.h>
 
-
+//线程1执行函数
 void th_entry(void *parameter)
 {
     while (1)
@@ -19,6 +19,7 @@ void th_entry(void *parameter)
     }
 }
 
+//线程2执行函数
 void th_entry2(void *parameter)
 {
     while (1)
@@ -26,4 +27,9 @@ void th_entry2(void *parameter)
         rt_kprintf("Hello 2...\n");
         rt_thread_mdelay(2000);
     }
+}
+
+//线程调度器hook函数
+void scheduler_hook(struct rt_thread *from, struct rt_thread *to){
+    rt_kprintf("from:%s ---> to:%s\n", from->name, to->name);
 }
