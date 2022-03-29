fcd8ff811aa2905da37cedb14cece76a028b96a1
KaiJin <ijink@qq.com>
2022-03-29 15:02:25 +0800

    2022/3/29

--- rt-thread-demo/applications/main.c
+++ rt-thread-demo/applications/main.c
@@ -26,24 +26,36 @@ rt_err_t ret;
 
 int main(void)
 {
+    //线程调度器hook func set
+    rt_scheduler_sethook(scheduler_hook);
+
     //动态创建线程
+    //线程1创建
     th1_ptr = rt_thread_create("th_demo1", th_entry, NULL, 1024, 20, 5);
+    //线程1创建成功性检查
     if (th1_ptr == RT_NULL)
     {
         LOG_E("rt_thread1 create failed...\n");
+        
         return -RT_ENOMEM;
     }
     LOG_D("rt_thread create1 success...\n");
+    //线程1启动
     rt_thread_startup(th1_ptr);
 
     //静态创建线程
-    ret = rt_thread_init(&th2, "th2_demo", th_entry2, NULL, th2_stack, 512, 15, 5);
+    //线程2创建
+    ret = rt_thread_init(&th2, "th_demo2", th_entry2, NULL, th2_stack, 512, 15, 5);
+    //线程2创建成功性检查
     if (ret < 0)
     {
         LOG_E("rt_thread2 create failed...\n");
         return ret;
     }
     LOG_D("rt_thread create2 success...\n");
+    //线程2启动
     rt_thread_startup(&th2);
+
+
     return RT_EOK;
 }
--- rt-thread-demo/applications/my_thread.h
+++ rt-thread-demo/applications/my_thread.h
@@ -10,8 +10,14 @@
 #ifndef APPLICATIONS_MY_THREAD_H_
 #define APPLICATIONS_MY_THREAD_H_
 
+//线程1执行函数
 void th_entry(void *parameter);
+
+//线程2执行函数
 void th_entry2(void *parameter);
+
+//线程调度器hook函数
+void scheduler_hook(struct rt_thread *from, struct rt_thread *to);
 
 
 #endif /* APPLICATIONS_MY_THREAD_H_ */
