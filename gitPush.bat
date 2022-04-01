38039ad63caced20dc5bd14cd488fc887b51485b
KaiJin <ijink@qq.com>
2022-04-01 14:31:21 +0800

    no

--- gitPush.bat
+++ gitPush.bat
@@ -5,5 +5,8 @@ git.exe add -A
 git.exe commit -m "%commit%"
  
 git push origin main
+call :ColorText 0C "-----------gitee push ok-----------"
 git push github main
+call :ColorText 0C "-----------github push ok-----------"
+
 pause
