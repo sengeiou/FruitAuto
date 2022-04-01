9294e14b36f1938d32cf15f0dffe22bf2bb3ae20
KaiJin <ijink@qq.com>
2022-04-01 14:38:09 +0800

    k

--- gitPush.bat
+++ gitPush.bat
@@ -5,19 +5,8 @@ git.exe add -A
 git.exe commit -m "%commit%"
  
 git push origin main
-:start
-echo.
-call :ColorText 0C "-----------gitee push ok-----------"
-echo.
+echo "color 02" -----------gitee push ok-----------
 git push github main
-call :ColorText 0C "-----------github push ok-----------"
-goto :eof
+call :ColorStr 0C "-----------github push ok-----------"
+
 pause
-
-
-:ColorText
-echo off
-<nul set /p ".=%DEL%" > "%~2"
-findstr /v /a:%1 /R "^$" "%~2" nul
-del "%~2" > nul 2>&1
-goto :eof
