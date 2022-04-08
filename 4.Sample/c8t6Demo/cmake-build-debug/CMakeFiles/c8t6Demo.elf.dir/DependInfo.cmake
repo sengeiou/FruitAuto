aff46cf8f3ffd2468d0d18d6a1969f47cefbe132
KaiJin <ijink@qq.com>
2022-04-08 13:09:57 +0800

    add kinds of demo

---
+++ 4.Sample/c8t6Demo/cmake-build-debug/CMakeFiles/c8t6Demo.elf.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/startup/startup_stm32f103xb.s" "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/cmake-build-debug/CMakeFiles/c8t6Demo.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Core/Src/main.c" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Core/Src/stm32f1xx_it.c" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Core/Src/syscalls.c" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Core/Src/system_stm32f1xx.c" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "C:/Users/ijink/OneDrive/fruit/4.Sample/c8t6Demo/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/c8t6Demo.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
